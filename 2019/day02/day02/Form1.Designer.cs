5306066c3a35ab12afd21301009ce17d00fd2d84
Thaís Oliveira <70452503+toliveirac@users.noreply.github.com>
2020-09-17 23:31:38 -0300

    Add files via upload

---
+++ 2019/day02/day02/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace day02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLeia = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtQnt = new System.Windows.Forms.TextBox();
+            this.txtSubs = new System.Windows.Forms.TextBox();
+            this.txtVerbo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnLeia
+            // 
+            this.btnLeia.Location = new System.Drawing.Point(21, 12);
+            this.btnLeia.Name = "btnLeia";
+            this.btnLeia.Size = new System.Drawing.Size(122, 34);
+            this.btnLeia.TabIndex = 0;
+            this.btnLeia.Text = "leia";
+            this.btnLeia.UseVisualStyleBackColor = true;
+            this.btnLeia.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(21, 65);
+            this.txtInput.MaxLength = 6000000;
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(523, 180);
+            this.txtInput.TabIndex = 1;
+            // 
+            // txtQnt
+            // 
+            this.txtQnt.Location = new System.Drawing.Point(561, 65);
+            this.txtQnt.Multiline = true;
+            this.txtQnt.Name = "txtQnt";
+            this.txtQnt.Size = new System.Drawing.Size(87, 42);
+            this.txtQnt.TabIndex = 1;
+            // 
+            // txtSubs
+            // 
+            this.txtSubs.Location = new System.Drawing.Point(561, 136);
+            this.txtSubs.Name = "txtSubs";
+            this.txtSubs.Size = new System.Drawing.Size(100, 20);
+            this.txtSubs.TabIndex = 2;
+            // 
+            // txtVerbo
+            // 
+            this.txtVerbo.Location = new System.Drawing.Point(561, 163);
+            this.txtVerbo.Name = "txtVerbo";
+            this.txtVerbo.Size = new System.Drawing.Size(100, 20);
+            this.txtVerbo.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtVerbo);
+            this.Controls.Add(this.txtSubs);
+            this.Controls.Add(this.txtQnt);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.btnLeia);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLeia;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtQnt;
+        private System.Windows.Forms.TextBox txtSubs;
+        private System.Windows.Forms.TextBox txtVerbo;
+    }
+}
+
---
+++ 2019/day02/day02/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace day02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string num_ = "0";
+            int substantivo,verbo = 0;
+            string[] numero = new string[500];
+            bool stat = false;
+
+            for (int item = 0; item < 99; item++)
+            {
+                if(stat == true) { break; }
+
+                substantivo = item;
+
+                for (int itens = 0; itens < 99; itens++)
+                {
+                    verbo = itens;
+
+                    string row = "0";
+                    int A, B, R, index = 0;
+                    
+                    System.IO.StreamReader file = new System.IO.StreamReader(@"C:\Users\toliv\Desktop\AdventOfCode\Day02\input.txt");
+
+                    row = file.ReadLine();
+
+                    string original = row;
+                    char[] delimitadores = new char[] { ',' };
+                    string[] strings = original.Split(delimitadores);
+
+                    //Separa cada numero em uma posição do array
+                    foreach (string s in strings)
+                    {
+                        if (index == 1) { numero[index] = substantivo.ToString(); index++; }
+                        else if (index == 2) { numero[index] = verbo.ToString(); index++; }
+                        else
+                        {
+                            numero[index] = s;
+                            index++;
+                        }
+                    }
+
+                    index = 0;
+
+                    //acessa cada pos no array e aplica regras
+                    foreach (string s in strings)
+                    {
+
+                        if (numero[index] == "99")  // Interrompe programa
+                        {
+                         if (numero[0] == "19690720")
+                         {
+                               txtSubs.Text = ((substantivo * 100) + verbo).ToString();
+                               break;
+                         }
+                            break;
+                        }
+                        else if (numero[index] == "1" | numero[index] == "2") //Soma e Substitui ou Multiplica e Substitui
+                        {
+                            //Pega valor da posição indicada
+                            R = Convert.ToInt32(numero[index + 3]);
+                            int nameA = Convert.ToInt32(numero[index + 1]);
+                            int nameB = Convert.ToInt32(numero[index + 2]);
+                            A = Convert.ToInt32(numero[nameA]);
+                            B = Convert.ToInt32(numero[nameB]);
+
+                            if (numero[index] == "1")
+                            {
+                                numero[R] = (A + B).ToString();
+                                index = index + 4;
+
+                            }
+                            else
+                            {
+                                numero[R] = (A * B).ToString();
+                                index = index + 4;
+                            }
+                        }
+
+                    }
+                    num_ = numero[0];
+                    index = 0;
+                    if (numero[0] == "19690720")
+                    {
+                        txtSubs.Text = ((substantivo * 100) + verbo).ToString();
+
+                        break;
+                    }
+
+
+                }
+              
+            }
+            txtInput.Text = "finishi";
+        }
+    }
+}
